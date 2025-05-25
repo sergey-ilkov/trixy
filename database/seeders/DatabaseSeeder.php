809aa7662769e90fe5f8fa0155a0f3e4be4cfd5a
Sergey <ilkov1103@gmail.com>
2025-05-25 22:48:02 +0300

    test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
             LanguageSeeder::class,
             // ReviewSeed::class,
 
-            // ActionSeeder::class,
+            ActionSeeder::class,
             // PostSeeder::class,
             ServiceCategorySeeder::class,
             // ServiceSeeder::class,
