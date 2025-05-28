215638cd886c2273aa698454ae2d28a7b9930eeb
Sergey <ilkov1103@gmail.com>
2025-05-28 09:50:40 +0300

    test

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -4,16 +4,19 @@
             <img width="140" height="62" class="footer-logo" src="{{ asset('images/logo.svg') }}" alt="logo">
             <ul class="footer-menu">
                 <li class="footer-menu__item">
-                    <a class="footer-menu__link {{ active_link('home') }}" href="{{ route('home') }}">Головна</a>
+                    <a class="footer-menu__link {{ active_link('home') }}" href="{{ route('home') }}">{{ __('header.links.home') }}</a>
                 </li>
                 <li class="footer-menu__item">
-                    <a class="footer-menu__link {{ active_link('services') }}" href="{{ route('services') }}">Кредитні сервіси</a>
+                    <a class="footer-menu__link {{ active_link('services') }}" href="{{ route('services') }}">{{ __('header.links.services') }}</a>
                 </li>
                 <li class="footer-menu__item">
-                    <a class="footer-menu__link {{ active_link('blog') }}" href="{{ route('blog') }}">Блог</a>
+                    <a class="footer-menu__link {{ active_link('blog') }}" href="{{ route('blog') }}">{{ __('header.links.blog') }}</a>
                 </li>
                 <li class="footer-menu__item">
-                    <a class="footer-menu__link {{ active_link('about') }}" href="{{ route('about') }}">Про нас</a>
+                    <a class="footer-menu__link {{ active_link('about') }}" href="{{ route('about') }}">{{ __('header.links.about') }}</a>
+                </li>
+                <li class="footer-menu__item">
+                    <a class="footer-menu__link {{ active_link('contacts') }}" href="{{ route('contacts') }}">{{ __('header.links.contacts') }}</a>
                 </li>
             </ul>
 
